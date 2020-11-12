5eb3f029263c25112c1a813524010594856665c7
RyuseiNono <ryuno72444za@gmail.com>
2020-11-12 12:23:31 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,25 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    # paramsからidをとって、その部屋のデータをモデルから引っ張ってる
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to action: room_messages_path(@room)
+    else
+      render action: :index
+      # binding.pry
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    # ストロングパラメータの後に、current_user.idを追加している
+  end
+
 end
