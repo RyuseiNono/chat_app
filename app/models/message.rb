b96671a0f962f90a45f51c028c0c5ed335a237a3
RyuseiNono <ryuno72444za@gmail.com>
2020-11-13 12:47:29 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,8 @@ class Message < ApplicationRecord
   has_one_attached :image
 
   validates :content,presence: true,unless: :was_attached?
-end
 
-def was_attached?
-  self.image.attached?
+  def was_attached?
+    self.image.attached?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :messages
 
   validates :name,presence: true
-        end
+
+end
